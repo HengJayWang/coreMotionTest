5d1d8f793d7d4762cd9ef8575c222c9f938acbdc
王亨傑 <hengjay.wang@itri.org.tw>
2019-02-18 17:09:53 +0800

    Initial Commit

---
+++ coreMotionTest/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  coreMotionTest
+//
+//  Created by Ｍ200_Macbook_Pro on 2019/2/11.
+//  Copyright © 2019 ITRI. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+    
+    let motionManager = CMMotionManager()
+//    let sensorRecorder = CMSensorRecorder()
+    var timer: Timer!
+
+    @IBOutlet weak var recordBtn: UIButton!
+    @IBOutlet weak var waveformView: WaveformView!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    let green = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+    let red = #colorLiteral(red: 0.9774432778, green: 0, blue: 0, alpha: 1)
+    var isRecord = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadUI()
+        
+        motionManager.accelerometerUpdateInterval = 0.01
+       
+//        sensorRecorder.recordAccelerometer(forDuration: 0.2)
+        
+    }
+    
+    func loadUI() {
+        setBtnCorner(btn: recordBtn)
+    }
+    
+    func setBtnCorner(btn: UIButton) {
+        btn.layer.shadowColor = UIColor.black.cgColor
+        btn.layer.shadowOffset = CGSize(width: 0.0, height: 5.0)
+        btn.layer.masksToBounds = false
+        btn.layer.shadowRadius = 2.0
+        btn.layer.shadowOpacity = 0.5
+        btn.layer.cornerRadius = 20.0
+    }
+    
+    
+    @IBAction func recordAccelerData(_ sender: UIButton) {
+        if isRecord {
+            isRecord.toggle()
+            recordBtn.setTitle("Start!", for: .normal)
+            recordBtn.backgroundColor = green
+            motionManager.stopDeviceMotionUpdates()
+            timer.invalidate()
+        } else {
+            isRecord.toggle()
+            recordBtn.setTitle("Recording ...", for: .normal)
+            recordBtn.backgroundColor = red
+            motionManager.startDeviceMotionUpdates()
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func update() {
+//        if let accelerometerData = motionManager.accelerometerData {
+//            print("accelerometerData: ")
+//            print(accelerometerData.acceleration)
+//        }
+//        if let gyroData = motionManager.gyroData {
+//            print("gyroData: ")
+//            print(gyroData.rotationRate)
+//        }
+        if let deviceMotion = motionManager.deviceMotion {
+//            print("userAcceleration in deviceMotion:")
+//            print(deviceMotion.userAcceleration)
+            
+            waveformView.pushSignal1BySliding(newValue: CGFloat(deviceMotion.userAcceleration.x))
+            waveformView.pushSignal2BySliding(newValue: CGFloat(deviceMotion.userAcceleration.y))
+            waveformView.pushSignal3BySliding(newValue: CGFloat(deviceMotion.userAcceleration.z))
+            
+            if (waveformView.signal1Index % 100 == 0) {
+                xLabel.text = String(format: "X: Max: %5f , min: %5f ",
+                                     waveformView.signal1Max, waveformView.signal1Min)
+                yLabel.text = String(format: "Y: Max: %5f , min: %5f ",
+                                     waveformView.signal2Max, waveformView.signal2Min)
+                zLabel.text = String(format: "Z: Max: %5f , min: %5f ",
+                                     waveformView.signal3Max, waveformView.signal3Min)
+            }
+            
+        }
+    }
+}
+
---
+++ coreMotionTest/WaveformView.swift
@@ -0,0 +1,201 @@
+//
+//  WaveformView.swift
+//  coreMotionTest
+//
+//  Created by Ｍ200_Macbook_Pro on 2019/2/18.
+//  Copyright © 2019 ITRI. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class WaveformView: UIView {
+
+    private struct Parameters {
+        static let backgroundOutlineWidth: CGFloat = 8.0
+        static let middleSeperateLineWidth: CGFloat = 2.5
+        static let signalLineWidth: CGFloat = 2.0
+        static let signalArrayLength = 1000
+        static let indexToUpdateView = signalArrayLength / 10
+    }
+    
+    var signal1Min: CGFloat = 0
+    var signal1Max: CGFloat = 0
+    var signal1Index: Int = 0
+    
+    var signal1Data = [CGFloat](repeating: 1.0, count: Parameters.signalArrayLength) {
+        didSet {
+            if (signal1Index + 1) % Parameters.indexToUpdateView == 0 {
+                setNeedsDisplay()
+                print("signal1Index: \(signal1Index), signal1Max: \(signal1Max), signal1Min: \(signal1Min)")
+            }
+        }
+    }
+    
+    
+    
+    var signal2Min: CGFloat = 0
+    var signal2Max: CGFloat = 0
+    var signal2Index: Int = 0
+    
+    var signal2Data = [CGFloat](repeating: 1.0, count: Parameters.signalArrayLength) {
+        didSet {
+            if (signal2Index + 1) % Parameters.indexToUpdateView == 0 { setNeedsDisplay() }
+        }
+    }
+    
+    var signal3Min: CGFloat = 0
+    var signal3Max: CGFloat = 0
+    var signal3Index: Int = 0
+    
+    var signal3Data = [CGFloat](repeating: 1.0, count: Parameters.signalArrayLength) {
+        didSet {
+            if (signal3Index + 1) % Parameters.indexToUpdateView == 0 { setNeedsDisplay() }
+        }
+    }
+    
+    
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        drawWaveformBackground(rect: rect)
+        
+        drawSignal(rect: rect)
+    }
+    
+    func drawWaveformBackground (rect: CGRect) {
+        // set black background
+        let background = UIBezierPath(rect: rect)
+        UIColor.black.setFill()
+        background.fill()
+        
+        // draw gray middle seperate line
+        let seperateLine1 = UIBezierPath()
+        //set the path's line width to the height of the stroke
+        seperateLine1.lineWidth = Parameters.middleSeperateLineWidth
+        
+        // move the initial point of the path
+        // to the start of the horizontal stroke
+        seperateLine1.move(to: CGPoint(x: 0, y: bounds.height / 3))
+        
+        // add a point to the path at the end of the stroke
+        seperateLine1.addLine(to: CGPoint(x: bounds.width, y: bounds.height / 3))
+        UIColor.gray.setStroke()
+        seperateLine1.stroke()
+        
+        // draw gray middle seperate line
+        let seperateLine2 = UIBezierPath()
+        //set the path's line width to the height of the stroke
+        seperateLine2.lineWidth = Parameters.middleSeperateLineWidth
+        
+        // move the initial point of the path
+        // to the start of the horizontal stroke
+        seperateLine2.move(to: CGPoint(x: 0, y: (bounds.height * 2) / 3))
+        
+        // add a point to the path at the end of the stroke
+        seperateLine2.addLine(to: CGPoint(x: bounds.width, y: (bounds.height * 2) / 3))
+        UIColor.gray.setStroke()
+        seperateLine2.stroke()
+        
+        // draw outline of waveform area
+        let outline = UIBezierPath(rect: rect)
+        outline.lineWidth = Parameters.backgroundOutlineWidth
+        let outlineColor = #colorLiteral(red: 1, green: 0.8820928931, blue: 0, alpha: 1)
+        outlineColor.setStroke()
+        outline.stroke()
+        
+    }
+    
+    func drawSignal (rect: CGRect) {
+        
+        let graphWidth: CGFloat = 0.9  // Graph is 90% of the width of the view
+        let width = bounds.width
+        let height = bounds.height
+        let stepX = (width * graphWidth) / CGFloat(signal1Data.count - 1)
+        
+        //draw waveform of signal1
+        let origin = CGPoint(x: width * (1 - graphWidth) / 2, y: height * 0.3 )
+        
+        let signal1 = UIBezierPath()
+        signal1.lineWidth = Parameters.signalLineWidth
+        let ch1Min = signal1Data.min()!
+        let ch1Scale = (signal1Data.max()! - ch1Min + 0.01)
+        signal1Min = ch1Min
+        signal1Max = ch1Scale + ch1Min - 1
+        
+        signal1.move(to: CGPoint(x: origin.x,
+                                 y: origin.y - height * 0.27 * (signal1Data[0] - ch1Min) / ch1Scale))
+        
+        for i in 1...signal1Data.count-1 {
+            signal1.addLine(to: CGPoint(x: origin.x + stepX * CGFloat(i),
+                                        y: origin.y - height * 0.27 * (signal1Data[i] - ch1Min) / ch1Scale))
+        }
+        UIColor.red.setStroke()
+        signal1.stroke()
+        
+        //draw waveform of signal2
+        let origin2 = CGPoint(x: width * (1 - graphWidth) / 2, y: height * 0.63 )
+        let signal2 = UIBezierPath()
+        signal2.lineWidth = Parameters.signalLineWidth
+        let ch2Min = signal2Data.min()!
+        let ch2Scale = (signal2Data.max()! - ch2Min + 0.01)
+        signal2Min = ch2Min
+        signal2Max = ch2Scale + ch2Min - 1
+        
+        signal2.move(to: CGPoint(x: origin2.x,
+                                 y: origin2.y - height * 0.27 * (signal2Data[0] - ch2Min) / ch2Scale))
+        
+        for i in 1...signal2Data.count-1 {
+            signal2.addLine(to: CGPoint(x: origin2.x + stepX * CGFloat(i),
+                                        y: origin2.y - height * 0.27 * (signal2Data[i] - ch2Min) / ch2Scale))
+        }
+        UIColor.green.setStroke()
+        signal2.stroke()
+        
+        //draw waveform of signal2
+        let origin3 = CGPoint(x: width * (1 - graphWidth) / 2, y: height * 0.96 )
+        let signal3 = UIBezierPath()
+        signal3.lineWidth = Parameters.signalLineWidth
+        let ch3Min = signal3Data.min()!
+        let ch3Scale = (signal3Data.max()! - ch3Min + 0.01)
+        signal3Min = ch3Min
+        signal3Max = ch3Scale + ch3Min - 1
+        
+        signal3.move(to: CGPoint(x: origin3.x,
+                                 y: origin3.y - height * 0.27 * (signal3Data[0] - ch3Min) / ch3Scale))
+        
+        for i in 1...signal3Data.count-1 {
+            signal3.addLine(to: CGPoint(x: origin3.x + stepX * CGFloat(i),
+                                        y: origin3.y - height * 0.27 * (signal3Data[i] - ch3Min) / ch3Scale))
+        }
+        UIColor.cyan.setStroke()
+        signal3.stroke()
+        
+    }
+    
+//    func pushSignal1 (newValue: CGFloat) {
+//        signal1Data.append(newValue)
+//        signal1Data.removeFirst()
+//    }
+    
+    func pushSignal1BySliding (newValue: CGFloat) {
+        signal1Data[signal1Index] = newValue
+        signal1Index = (signal1Index == signal1Data.count - 1) ? 0 : (signal1Index + 1)
+    }
+    
+//    func pushSignal2 (newValue: CGFloat) {
+//        signal2Data.append(newValue)
+//        signal2Data.removeFirst()
+//    }
+    
+    func pushSignal2BySliding (newValue: CGFloat) {
+        signal2Data[signal2Index] = newValue
+        signal2Index = (signal2Index == signal2Data.count - 1) ? 0 : (signal2Index + 1)
+    }
+
+    func pushSignal3BySliding (newValue: CGFloat) {
+        signal3Data[signal3Index] = newValue
+        signal3Index = (signal3Index == signal3Data.count - 1) ? 0 : (signal3Index + 1)
+    }
+    
+}
